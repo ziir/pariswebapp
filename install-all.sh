d9228b2bcd3597dbec39ed8c22ea3472e9709bd8
Julien Wajsberg <felash@gmail.com>
2018-09-26 22:18:45 +0200

    Move husky back to the root package.json

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-for i in */ ; do
+echo ">>> Running 'yarn install' in the root directory"
+yarn install
+
+for i in react-*/ ; do
   echo ">>> Running 'yarn install' in '$i'"
   (cd "$i" && yarn install)
 done
