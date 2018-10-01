21e30001f6400b7cce4c055018511db195f69b5e
Timothée Pillard <tim@gandi.net>
2018-10-01 14:34:54 +0200

    react-3: Re-render whole app only if agenda has changed.
    Use the Last-Modified response header as source of truth.
    Always re-render the whole app if the Last-Modified header can not be retrieved
    or is empty.

--- react-3/src/App.js
+++ react-3/src/App.js
@@ -15,6 +15,7 @@ type State = {|
 
 class App extends Component<{||}, State> {
   _timeoutId: TimeoutID | null = null;
+  _LastModified: string | null;
   state = { agenda: null };
 
   componentDidMount() {
@@ -37,10 +38,22 @@ class App extends Component<{||}, State> {
         );
       }
       const result = await fetchResponse.json();
-      this.setState({ agenda: result });
+
+      let LastModified;
+      try {
+        LastModified = fetchResponse.headers.get('Last-Modified');
+      } catch (e) {
+        LastModified = null;
+        console.error('Unable to retrieve Last-Modified response header.', e);
+      }
+      if (!LastModified || LastModified !== this._LastModified) {
+        this.setState({ agenda: result });
+        this._LastModified = LastModified;
+      }
     } catch (e) {
       console.error('Got an error while fetching the agenda.', e);
     }
+
     this._timeoutId = setTimeout(() => this.fetchData(), FETCH_INTERVAL_MS);
   }
 
