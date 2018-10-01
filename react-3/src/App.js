61f0482d4332ce9e9ed983ebfdf56d8f7a7b2eb2
Timothée Pillard <tim@gandi.net>
2018-10-01 14:25:11 +0200

    Add react-3 based on react-2

---
+++ react-3/src/App.js
@@ -0,0 +1,62 @@
+// @flow
+
+import React, { Component } from 'react';
+
+import PresentationList, { type Agenda } from './PresentationList';
+
+import logo from './logo.svg';
+import './App.css';
+
+const FETCH_INTERVAL_MS = 5000;
+
+type State = {|
+  agenda: Agenda | null,
+|};
+
+class App extends Component<{||}, State> {
+  _timeoutId: TimeoutID | null = null;
+  state = { agenda: null };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentWillUnmount() {
+    if (this._timeoutId !== null) {
+      clearTimeout(this._timeoutId);
+      this._timeoutId = null;
+    }
+  }
+
+  async fetchData() {
+    try {
+      const fetchResponse = await fetch('/agenda.json');
+      if (!fetchResponse.ok) {
+        throw new Error(
+          `Got an error HTTP Response with status ${fetchResponse.status}`
+        );
+      }
+      const result = await fetchResponse.json();
+      this.setState({ agenda: result });
+    } catch (e) {
+      console.error('Got an error while fetching the agenda.', e);
+    }
+    this._timeoutId = setTimeout(() => this.fetchData(), FETCH_INTERVAL_MS);
+  }
+
+  render() {
+    const { agenda } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Paris Web App</h1>
+        </header>
+        {agenda ? <PresentationList agenda={agenda} /> : null}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-3/src/ListItem.js
@@ -0,0 +1,82 @@
+// @flow
+
+import React from 'react';
+
+export type Attending = boolean | null;
+export type Location = 'small' | 'big' | 'unknown';
+export type Day = 'thursday' | 'friday' | 'saturday';
+export type ConferenceData = {|
+  +title: string,
+  +speakers: Array<string>,
+  +location: Location,
+  +day: Day,
+  +year: number,
+  +start: string,
+  +end: string,
+|};
+
+type Props = {|
+  +index: number,
+  +attending: Attending,
+  +entry: ConferenceData,
+  +handleChange: (attending: Attending) => any,
+|};
+
+function ListItem(props: Props) {
+  return (
+    <div>
+      <h3>{props.entry.title}</h3>
+      <ul>
+        {props.entry.speakers.map(speaker => (
+          <li>{speaker}</li>
+        ))}
+      </ul>
+      <p>
+        This presentation will take place:
+        <br />- on{' '}
+        <em>
+          {props.entry.day} {props.entry.start} - {props.entry.end}
+        </em>
+        <br />- in room <strong>{props.entry.location}</strong>
+      </p>
+      <fieldset>
+        <legend>Attend?</legend>
+        <label>
+          <input
+            type="radio"
+            id={`${props.index}-no`}
+            name={props.index}
+            value="no"
+            checked={props.attending === false}
+            onChange={props.handleChange.bind(null, false)}
+          />
+          No
+        </label>
+        <label>
+          <input
+            type="radio"
+            id={`${props.index}-maybe`}
+            name={props.index}
+            value="maybe"
+            checked={props.attending === null}
+            onChange={props.handleChange.bind(null, null)}
+          />
+          Maybe
+        </label>
+        <label>
+          <input
+            type="radio"
+            id={`${props.index}-yes`}
+            name={props.index}
+            value="yes"
+            checked={props.attending === true}
+            onChange={props.handleChange.bind(null, true)}
+          />
+          Yes
+        </label>
+      </fieldset>
+    </div>
+  );
+}
+
+export default ListItem;
