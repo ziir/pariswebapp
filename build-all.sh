09646dac01e99f87ae6cb89dc03acd1222d777a4
Timothée Pillard <tim@gandi.net>
2018-10-02 01:17:24 +0200

    Remove unnecessary yarn install --dev, package-lock

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for i in react-*/ ; do
-  echo ">>> Running 'yarn install --dev && yarn build' in '$i'"
-  (cd "$i" && yarn install --dev && yarn build)
+  echo ">>> Running 'yarn install && yarn build' in '$i'"
+  (cd "$i" && yarn install && yarn build)
 done
