c3627fbe41cf6a649475ebd9186c57c77090be84
Julien Wajsberg <felash@gmail.com>
2018-10-02 14:39:23 +0200

    Adapt changes to react-3

--- react-3/src/PresentationList.js
+++ react-3/src/PresentationList.js
@@ -2,15 +2,11 @@
 
 import memoize from 'memoize-one';
 import React, { Component, Fragment } from 'react';
-import ListItem, {
-  type Attending,
-  type ConferenceData,
-  type Day,
-} from './ListItem';
+import ListItem, { type Attending } from './ListItem';
 import InputField from './InputField';
 import ValueChooser from './ValueChooser';
 
-export type Agenda = $ReadOnlyArray<ConferenceData>;
+import type { Agenda, Day } from './types';
 
 type Props = {|
   +agenda: Agenda,
