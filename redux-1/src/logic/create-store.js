523c4fd07e4fa52dd07e6f8776fd82f7fa9ec9f4
Julien Wajsberg <felash@gmail.com>
2018-10-03 18:11:37 +0200

    Add debugging tools

--- redux-1/src/logic/create-store.js
+++ redux-1/src/logic/create-store.js
@@ -1,10 +1,31 @@
 // @flow
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import reducers from '../reducers';
 
 import type { Store } from '../types/store';
 
 export default function createAppStore(): Store {
-  return createStore(reducers);
+  const middlewares = [];
+
+  if (process.env.NODE_ENV === 'development') {
+    middlewares.push(
+      createLogger({
+        collapsed: true,
+        titleFormatter: (action, time, duration) =>
+          `[action]    ${action.type} (in ${duration.toFixed(2)} ms)`,
+        logErrors: false,
+        duration: true,
+      })
+    );
+  }
+
+  const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
+  return store;
 }
